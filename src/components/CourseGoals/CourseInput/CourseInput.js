942715aada14927d70bec3b8b4db8a4cdf36f690
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-29 12:51:22 +0200

    completed implementing styled components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,7 +26,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl>
+      <FormControl invalid={!isValid}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
@@ -43,12 +43,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => (props.invalid  ? 'red' : 'black')}
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => (props.invalid  ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? 'salmon' : 'transparent')};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -58,15 +60,6 @@ const FormControl = styled.div`
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: salmon;
-  }
-
-  &.invalid label {
-    color: red;
   }
 `;
 
